79a2f0160624cdf4777379b74e994837fd7f9ebb
Mario Velasco Alonso <mario1velasco@gmail.com>
2018-03-07 10:07:04 +0100

    Feat: Check input name errors

--- src/app/components/users/user-form/user-form.component.ts
+++ src/app/components/users/user-form/user-form.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {  Component} from '@angular/core';
+import { User } from './../../../shared/models/user.model';
+
 
 @Component({
   selector: 'app-user-form',
   templateUrl: './user-form.component.html',
   styleUrls: ['./user-form.component.css']
 })
-export class UserFormComponent implements OnInit {
+export class UserFormComponent {
+  user: User = new User();
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  // onSubmitUserForm(user){
+  //   this.user=user;
+  //   console.log(this.user);
+    
+  // }
 }
