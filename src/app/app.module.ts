669f971770414676995742f53b2d20f038cf9ae6
Mario Velasco Alonso <mario1velasco@gmail.com>
2018-03-07 16:33:40 +0100

    Feat: Create componet user-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { UserFormComponent } from './components/users/user-form/user-form.component';
 import { UserItemComponent } from './components/users/user-item/user-item.component';
+import { UserListComponent } from './components/users/user-list/user-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     UserFormComponent,
-    UserItemComponent
+    UserItemComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
