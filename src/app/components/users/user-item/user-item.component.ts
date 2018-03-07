32c9f3a9a021a0fcbbf3165e360d2859db6bf376
Mario Velasco Alonso <mario1velasco@gmail.com>
2018-03-07 10:59:49 +0100

    Feat: Pass user to another component works

--- src/app/components/users/user-item/user-item.component.ts
+++ src/app/components/users/user-item/user-item.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { User } from './../../../shared/models/user.model';
 
 @Component({
   selector: 'app-user-item',
   templateUrl: './user-item.component.html',
   styleUrls: ['./user-item.component.css']
 })
-export class UserItemComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class UserItemComponent {
+  @Input() user: User = new User();
 
 }
